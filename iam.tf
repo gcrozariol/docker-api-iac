298e44a2a1f563e90d6c18b9bb5361bde623a81e
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-07-01 14:53:35 -0300

    refactor: add token permission

--- iam.tf
+++ iam.tf
@@ -10,18 +10,49 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "terraform-role" {
+  name = "terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Principal : {
+          Federated : "arn:aws:iam::269624241181:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:gcrozariol/docker-api-iac:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
   assume_role_policy = jsonencode({
-    Version: "2012-10-17",
-    Statement: [
+    Version : "2012-10-17",
+    Statement : [
       {
-        Effect: "Allow",
-        Principal: {
-          Service: "build.apprunner.amazonaws.com"
+        Effect : "Allow",
+        Principal : {
+          Service : "build.apprunner.amazonaws.com"
         },
-        Action: "sts:AssumeRole"
+        Action : "sts:AssumeRole"
       }
     ]
   })
@@ -29,7 +60,7 @@ resource "aws_iam_role" "app-runner-role" {
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ]
-  
+
   tags = {
     IAC = "True"
   }
@@ -39,20 +70,20 @@ resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
   assume_role_policy = jsonencode({
-    Version: "2012-10-17",
-    Statement: [
+    Version : "2012-10-17",
+    Statement : [
       {
-        Effect: "Allow",
-        Action: "sts:AssumeRoleWithWebIdentity",
-        Principal: {
-          Federated: "arn:aws:iam::269624241181:oidc-provider/token.actions.githubusercontent.com"
+        Effect : "Allow",
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Principal : {
+          Federated : "arn:aws:iam::269624241181:oidc-provider/token.actions.githubusercontent.com"
         },
-        Condition: {
-          StringEquals: {
-            "token.actions.githubusercontent.com:aud": [
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" : [
               "sts.amazonaws.com"
             ],
-            "token.actions.githubusercontent.com:sub": [
+            "token.actions.githubusercontent.com:sub" : [
               "repo:gcrozariol/docker-api:ref:refs/heads/main"
             ]
           }
@@ -65,28 +96,28 @@ resource "aws_iam_role" "ecr-role" {
     name = "ecr-app-permission"
 
     policy = jsonencode({
-      Version: "2012-10-17",
-      Statement: [
+      Version : "2012-10-17",
+      Statement : [
         {
-          Sid: "Statement1"
-          Effect: "Allow",
-          Resource: "*",
-          Action: "apprunner:*"
+          Sid : "Statement1"
+          Effect : "Allow",
+          Resource : "*",
+          Action : "apprunner:*"
         },
         {
-          Sid: "Statement2"
-          Effect: "Allow",
-          Resource: "*",
-          Action: [
+          Sid : "Statement2"
+          Effect : "Allow",
+          Resource : "*",
+          Action : [
             "iam:PassRole",
             "iam:CreateServiceLinkedRole"
           ]
         },
         {
-          Sid: "Statement3"
-          Effect: "Allow",
-          Resource: "*"
-          Action: [
+          Sid : "Statement3"
+          Effect : "Allow",
+          Resource : "*"
+          Action : [
             "ecr:GetDownloadUrlForLayer",
             "ecr:BatchGetImage",
             "ecr:BatchCheckLayerAvailability",
